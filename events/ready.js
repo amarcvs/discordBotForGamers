67c19e6b56222aec5582791397cec42cf98bd8be
Marco Alberti <alberti.1794436@studenti.uniroma1.it>
2022-02-20 18:34:41 +0100

    bot activity

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,9 @@ module.exports = {
     name: "ready",
     execute() {
         console.log("ONLINE");
-    
+        
+        client.user.setActivity('Call of Duty 4', {type: 'PLAYING'});
+        
         let text = `**${client.users.cache.get(process.env.BOT_ID).tag}** is online`;
         writeLogs(text);
     }
